662c314075a86a6bb60c0ad912a0156fb213f7bf
sbonelondlela <nkosiyomuzisbonelo@gmail.com>
2023-02-20 10:44:32 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,1 @@
-#Include < stdio.h
-/**
- * main - A program that prints a line with puts function
- * Return: 0 (Success)
- */
-int main(void)
-{
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
-}
+swap -d
