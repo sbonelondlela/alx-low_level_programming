513f6fc1b1c032cc395ea105c7e730b3a66b3199
sbonelondlela <nkosiyomuzisbonelo@gmail.com>
2023-02-24 11:45:01 -0800

    2 Task

--- 0x03-debugging/2-main.c
+++ 0x03-debugging/2-main.c
@@ -8,14 +8,12 @@
 
 int main(void)
 {
-        int a, b, c;
+        int (a, b, c);
         int largest;
 
-        a = 972;
-        b = -98;
-        c = 0;
-
-        largest = largest_number(a, b, c);
+        (a = 972);
+        (b = -98);
+        (c = 0);
 
         printf("%d is the largest number\n", largest);
 
