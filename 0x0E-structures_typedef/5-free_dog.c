6e90795d28d842da463b378d1f94a67aa397d638
sbonelondlela <nkosiyomuzisbonelo@gmail.com>
2023-03-20 15:38:55 -0700

    Updates

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,18 +1,20 @@
 #include <stdlib.h>
-	#include "dog.h"
-
+#include "dog.h"
 
 /**
 * main - Entry point
+* Description: Struct dog
 * free_dog - frees memory allocated for a struct dog
 * @d: struct dog to free
+*
+* Return: always return 0 (seccess)
 */
-	void free_dog(dog_t *d)
-	{
-		if (d)
-		{
-			free(d->name);
-			free(d->owner);
-			free(d);
-		}
-	}
+void free_dog(dog_t *d)
+{
+if (d)
+{
+free(d->name);
+free(d->owner);
+free(d);
+}
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,27 +15,23 @@
 */
 struct dog
 {
-		char *name;
-		float age;
-		char *owner;
+char *name;
+float age;
+char *owner;
 };
-
 
 /**
 * main - Entry point
 * Description: dog_t - typedef for struct dog
 *
 */
-	typedef struct dog dog_t;
+typedef struct dog dog_t;
 
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
 
-	void init_dog(struct dog *d, char *name, float age, char *owner);
-	void print_dog(struct dog *d);
-	dog_t *new_dog(char *name, float age, char *owner);
-	void free_dog(dog_t *d);
-	char *_strcpy(char *dest, char *src);
-	int _strlen(char *s);
-
-
-	#endif
-
+#endif
